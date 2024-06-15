d9e31caf15e7163a8f3f4a914ad368dd8ea524ab
pritapuja <prita.pujaastuti@gmail.com>
2024-06-16 06:38:22 +0700

    first commit

--- config.js
+++ config.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBM-SC34hIiPeTg-zrerJfwH_qcl3vJKD4",
-  authDomain: "notesapp-193e6.firebaseapp.com",
-  projectId: "notesapp-193e6",
-  storageBucket: "notesapp-193e6.appspot.com",
-  messagingSenderId: "349427471065",
-  appId: "1:349427471065:web:cb978780fbf1227eece4c8",
-  measurementId: "G-3S2HYX6ST2"
+  
 }
 
 if (!firebase.apps.length) {
